66b29725144053be7d4f27d5e335830312fe1d1e
schcirrus <mciver.dave@gmail.com>
2018-09-03 10:19:03 +1200

    A test commit to Github
    Commit with all actions for articles apart from delete.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,10 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require bootstrap-sockets
 //= require activestorage
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sockets
 //= require turbolinks
 //= require_tree .
 
