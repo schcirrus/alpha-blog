29e127d4687ae1ddd591a16c5b3be98b398c8bd5
schcirrus <mciver.dave@gmail.com>
2018-09-06 16:04:02 +1200

    A test commit to Github
    Commit with all actions for articles apart from delete.
    
    Errors and flash messages styling added.
    
    Last commit before section 5 wrap up.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
             length: {minimum: 3, maximum: 25}
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: 25},
+  validates :email, presence: true, length: {maximum: 105},
             uniqueness: {case_sensitive: false},
             format: {with: VALID_EMAIL_REGEX}
 
