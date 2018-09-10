62b2a646899a5ca0e7cca66d02a242ce19060888
schcirrus <mciver.dave@gmail.com>
2018-09-11 11:42:45 +1200

    Restrict actions based on login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = 'Login sucessful'
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = 'Incorrect login information. Please try again'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'You have logged out'
+    redirect_to root_path
+  end
+end
